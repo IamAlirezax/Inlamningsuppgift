81a341e9e6b698caa2cea449c1027bf77c80961c
Alirezam <Samrad84@yahoo.com>
2020-08-31 11:28:31 +0200

    Initial commit

---
+++ Inlamningsuppgift/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inlamningsuppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
