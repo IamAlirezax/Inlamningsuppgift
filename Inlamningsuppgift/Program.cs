d872ac9f14df84af5fe0f4d250d15f92bcde4b49
Alirezam <Samrad84@yahoo.com>
2020-09-01 09:42:45 +0200

    Första Vecka

--- Inlamningsuppgift/Program.cs
+++ Inlamningsuppgift/Program.cs
@@ -6,7 +6,40 @@ namespace Inlamningsuppgift
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double c;
+            //Console.Write("Hello:");
+            //Console.WriteLine("Alireza Mohammadi");
+            //Console.Write("76+34:");
+            //Console.WriteLine(76 + 34);
+            Console.Write("skriv en operator : +,-,x eller/ :");
+            string d = Console.ReadLine();
+            Console.Write("skriv ett nummer:");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("skriv ett annat nummer:");
+            int b = int.Parse(Console.ReadLine());
+            if(d == "+")
+            {
+                c = a + b;
+                
+            }
+             else if (d == "-")
+            {
+                c = a - b;
+            }
+             else if (d == "x")
+            {
+                c = a * b;
+            }
+            else 
+            {
+                c = a / b;
+
+            }
+
+            Console.WriteLine(c);
+            Console.ReadLine();
+
+
         }
     }
 }
